8458ab0175b7e4cd06bba290a2ea665b095ff5a4
jaxs-ribs <luc.baracat@googlemail.com>
2024-04-23 16:20:27 +0200

    Use state

--- command_center/src/lib.rs
+++ command_center/src/lib.rs
@@ -1,21 +1,23 @@
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 use kinode_process_lib::{
-    await_message, call_init, get_blob, http, println, Address, ProcessId, Request,
-    Response, Message,
+    await_message, call_init, get_blob, http, println, Address, Message, ProcessId, Request,
+    Response,
 };
 
 mod structs;
 use structs::*;
+
+mod tg_api;
 
 wit_bindgen::generate!({
     path: "wit",
     world: "process",
 });
 
-fn handle_http_messages(message: &Message) {
+fn handle_http_messages(our: &Address, message: &Message, state: &mut Option<State>) {
     match message {
         Message::Response { .. } => {
             return;
@@ -37,7 +39,7 @@ fn handle_http_messages(message: &Message) {
             };
             match path.as_str() {
                 "/config" => {
-                    config(&body.bytes);
+                    config(our, &body.bytes, state);
                 }
                 _ => {
                     return;
@@ -47,25 +49,38 @@ fn handle_http_messages(message: &Message) {
     }
 }
 
-fn config(body_bytes: &[u8]) {
+fn config(our: &Address, body_bytes: &[u8], state: &mut Option<State>) {
     let Ok(initial_config) = serde_json::from_slice::<InitialConfig>(body_bytes) else {
         return;
     };
-    http::send_response(
-        http::StatusCode::OK,
-        Some(HashMap::from([(
-            "Content-Type".to_string(),
-            "application/json".to_string(),
-        )])),
-        b"{\"message\": \"success\"}".to_vec(),
-    );
-    println!("Initial config {:?}", initial_config);
+    match state {
+        Some(state_) => {
+            println!("Modifying state to {:?}", initial_config);
+            state_.config = initial_config;
+        }
+        None => {
+            println!("Creating state {:?}", initial_config);
+            *state = Some(State::new(our, initial_config));
+        }
+    }
+    if let Some(ref mut state) = state {
+        state.save();
+        http::send_response(
+            http::StatusCode::OK,
+            Some(HashMap::from([(
+                "Content-Type".to_string(),
+                "application/json".to_string(),
+            )])),
+            b"{\"message\": \"success\"}".to_vec(),
+        );
+    }
 }
 
 call_init!(init);
 fn init(our: Address) {
     println!("begin");
     let _ = http::serve_ui(&our, "ui/", true, false, vec!["/", "/config"]);
+    let mut state = State::fetch();
     loop {
         let Ok(message) = await_message() else {
             continue;
@@ -74,7 +89,7 @@ fn init(our: Address) {
             continue;
         }
         if message.source().process == "http_server:distro:sys" {
-            let http_request_outcome = handle_http_messages(&message);
+            let http_request_outcome = handle_http_messages(&our, &message, &mut state);
         }
     }
 }
--- command_center/src/structs.rs
+++ command_center/src/structs.rs
@@ -1,4 +1,71 @@
 use serde::{Deserialize, Serialize};
+use kinode_process_lib::{get_state, set_state, Address};
+use crate::tg_api::init_tg_bot;
+use serde::Deserializer;
+use serde::Serializer;
+use crate::tg_api::Api;
+
+#[derive(Debug)]
+pub struct State {
+    pub our: Address,
+    pub config: InitialConfig,
+    // Non-serializable fields
+    pub tg_api: Api,
+    pub tg_worker: Address,
+}
+
+impl State {
+    pub fn new(our: &Address, config: InitialConfig) -> Self {
+        hydrate_state(our, config).expect("Failed to hydrate state")
+    }
+
+    pub fn fetch() -> Option<State> {
+        if let Some(state_bytes) = get_state() {
+            bincode::deserialize(&state_bytes).ok()
+        } else {
+            None
+        }
+    }
+
+    pub fn save(&self) {
+        let serialized_state = bincode::serialize(self).expect("Failed to serialize state");
+        set_state(&serialized_state);
+    }
+}
+
+impl Serialize for State {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let serializable_part = (self.our.clone(), &self.config);
+        serializable_part.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for State {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let (our, config) = Deserialize::deserialize(deserializer)?;
+        Ok(hydrate_state(&our, config).expect("Failed to hydrate state"))
+    }
+}
+
+fn hydrate_state(our: &Address, config: InitialConfig) -> anyhow::Result<State> {
+    let Ok((tg_api, tg_worker)) =
+        init_tg_bot(our.clone(), &config.telegram_key, None)
+    else {
+        return Err(anyhow::anyhow!("tg bot couldn't boot."));
+    };
+    Ok(State {
+        our: our.clone(),
+        config, 
+        tg_api,
+        tg_worker,
+    })
+}
 
 #[derive(Serialize, Deserialize, Default, Debug, Clone)]
 pub struct InitialConfig {
