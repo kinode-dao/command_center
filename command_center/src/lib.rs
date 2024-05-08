85661feeb88531ba463ae489b0147dbae0e961eb
jaxs-ribs <luc.baracat@googlemail.com>
2024-05-08 11:46:31 +0200

    Remove begin

--- command_center/src/lib.rs
+++ command_center/src/lib.rs
@@ -197,8 +197,6 @@ fn init(our: Address) {
         panic!("Failed to spawn pkgs");
     };
 
-    println!("begin");
-
     loop {
         match handle_message(&our, &mut state, &pkgs) {
             Ok(_) => {}
