3ea94892167a30b987a2046ac1019de591d9cd02
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-12 14:40:31 +0200

    Interface for storage

---
+++ storage_interface/src/lib.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub type GlobalTweetMap = HashMap<String, TweetData>;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TweetData {
+    pub content: String,
+    pub photo: Option<String>,
+    pub likes: Option<i32>,
+    pub date: Option<i64>,
+    pub comments: Option<i32>,
+    pub retweets: Option<i32>,
+    pub views: Option<i32>,
+    pub user_likes_tweet: Option<bool>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Request {
+    GetTweets {
+        start_time: i64,
+        end_time: i64,
+    },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Response {
+    GetTweets {
+        tweets: GlobalTweetMap,
+    },
+}
---
+++ storage_interface/target/debug/deps/storage_interface-d34ca2571ee33e3c.d
@@ -0,0 +1,5 @@
+/Users/fresh/Documents/uqbar/command_center/storage_interface/target/debug/deps/libstorage_interface-d34ca2571ee33e3c.rmeta: src/lib.rs
+
+/Users/fresh/Documents/uqbar/command_center/storage_interface/target/debug/deps/storage_interface-d34ca2571ee33e3c.d: src/lib.rs
+
+src/lib.rs:
