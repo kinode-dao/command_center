a120c52af4c54ab33f5f52b0593552f9c57f26dc
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-19 12:18:12 +0200

    Add conversation

--- storage_interface/src/lib.rs
+++ storage_interface/src/lib.rs
@@ -1,6 +1,25 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Request {
+    GetTweets {
+        start_time: i64,
+        end_time: i64,
+    },
+    SendConversation(CurrentConversation),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Response {
+    GetTweets {
+        tweets: GlobalTweetMap,
+    },
+    SendConversation,
+}
+
+
 pub type GlobalTweetMap = HashMap<String, TweetData>;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -15,17 +34,11 @@ pub struct TweetData {
     pub user_likes_tweet: Option<bool>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Request {
-    GetTweets {
-        start_time: i64,
-        end_time: i64,
-    },
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct CurrentConversation {
+    pub title: Option<String>,
+    /// Note that every even number is going to be a question, and every odd number is going to be an answer
+    pub messages: Vec<String>,
+    pub date_created: Option<i64>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Response {
-    GetTweets {
-        tweets: GlobalTweetMap,
-    },
-}
