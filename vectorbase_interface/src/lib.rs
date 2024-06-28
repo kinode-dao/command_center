02341e50812a904a8ad73905d77425c52705751e
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-28 16:09:27 +0200

    Separate

--- vectorbase_interface/src/lib.rs
+++ vectorbase_interface/src/lib.rs
@@ -1,23 +1,43 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Request {
-    ListDatabases,
-    SubmitData {
-        database_name: String,
-        values: Vec<(String, String)>,
-    },
-    SemanticSearch {
-        database_name: String,
-        top_k: usize,
-        query: String,
-    },
+pub mod vectorbase {
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub enum Request {
+        ListDatabases,
+        SubmitData {
+            database_name: String,
+            values: Vec<(String, String)>,
+        },
+        SemanticSearch {
+            database_name: String,
+            top_k: usize,
+            query: String,
+        },
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub enum Response {
+        ListDatabases(Vec<String>),
+        SubmitData,
+        SemanticSearch(Vec<(String, String)>),
+        Error(String),
+    }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Response {
-    ListDatabases(Vec<String>),
-    SubmitData,
-    SemanticSearch(Vec<(String, String)>),
-    Error(String),
+pub mod rag {
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub enum Request {
+        RAG {prompt: String, rag_type: RAGType},
+    }
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub enum Response {
+        RAG(String),
+        Error(String),
+    }
+
+    pub enum RAGType {
+        Naive,
+        Vector,
+    }
 }
